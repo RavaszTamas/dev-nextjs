8c6b8cec7128fcbdc8c3b999fbddef569090c011
RavaszTamas <ravaszt88@gmail.com>
2023-05-24 18:01:15 +0300

    build fix

--- src/components/SecondaryTitle.tsx
+++ src/components/SecondaryTitle.tsx
@@ -1,7 +1,7 @@
-import {NewAlenia} from "@/assets/fonts";
 import React from "react";
+import {NewAlenia} from "@/assets/fonts";
 
-export default function SecondaryTitle({children, align = "right", textTransform="uppercase"}: { children: string, align?: "left" | "right" | "center",textTransform?:string }) {
+export default function SecondaryTitle({children, align = "right", textTransform="uppercase"}: { children: any, align?: "left" | "right" | "center",textTransform?:string }) {
 
     return(
         <h2 className={`text-primaryWhite ${NewAlenia.className} ${textTransform} text-[64px] leading-[74px] max-w-4xl font-bold ${align === "right" ? "text-right" : align === "center"? "text-center" : "text-left"}`}>
