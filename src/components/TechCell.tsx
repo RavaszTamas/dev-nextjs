142ebf273b5495b36ae7600d3c2e0867147b57d7
RavaszTamas <ravaszt88@gmail.com>
2023-05-30 01:14:00 +0300

    opacity transition description

--- src/components/TechCell.tsx
+++ src/components/TechCell.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import {NewAlenia} from "@/assets/fonts";
 
-export default function TechCell({number, title}: { number: string; title: string | React.ReactNode }) {
+export default function TechCell({number, title, description}: { number: string; title: string | React.ReactNode, description?:string }) {
 
-    return <div className="w-[410px] h-[400px] p-[1px] bg-gradient-to-br from-primaryOrange to-primaryPurple">
+    return <div className="group w-[410px] h-[400px] p-[1px] bg-gradient-to-br from-primaryOrange to-primaryPurple">
         <div className="bg-primaryBlack w-full h-full flex flex-col justify-between p-8">
-            <div className="text-primaryWhite bg-primaryPurple w-fit px-4 text-xl">{number}</div>
-            <div className={`text-primaryWhite ${NewAlenia.className} text-4xl`}>
+            <p className="text-primaryWhite bg-primaryPurple w-fit px-4 text-xl">{number}</p>
+            <p className="opacity-0 group-hover:opacity-100 text-primaryWhite transition transition-opacity">
+                {description}
+            </p>
+            <p className={`text-primaryWhite ${NewAlenia.className} text-4xl`}>
                 {title}
-            </div>
+            </p>
         </div>
     </div>
 }
